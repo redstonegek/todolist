4c6060a3f2bf8e5ef212aaa5c563c565aeaa9a4a
matilda <mrhaine@xs4all.nl>
2025-02-07 17:02:26 +0100

    moved some code

--- data.js
+++ data.js
@@ -1,18 +1,6 @@
-import { UserData } from "./dataTypes/userData.ts";
+import { UserData } from "./dataTypes/userData.js";
 
-function getData(Id, Recursive = false) {
-	const data = JSON.parse(Deno.readTextFileSync(`data/${getType(Id)}.json`));
-	const object = data[Id];
-	if (Recursive) {
-		if (object.child) {
-			const child = {};
-			object.ids.forEach((childId) => {
-				child[childId] = getData(childId, Recursive);
-			});
-			object.ids = child;
-		}
-	}
-}
+
 
 export class dataManager {
 	instances = {};
@@ -20,7 +8,9 @@ export class dataManager {
 		if (this.instances[id]) {
 			return this.instances[id];
 		}
+		// TODO: make a new instances
 	}
+	
 	getObj(id, Recursive = false) {
 		const type = getType(id);
 		switch (type) {
@@ -40,4 +30,18 @@ function getType(id) {
 	};
 	const shortType = id.slice(2)[0];
 	return typeObject[shortType];
+}
+
+function getData(Id, Recursive = false) {
+	const data = JSON.parse(Deno.readTextFileSync(`data/${getType(Id)}.json`));
+	const object = data[Id];
+	if (Recursive) {
+		if (object.child) {
+			const child = {};
+			object.ids.forEach((childId) => {
+				child[childId] = getData(childId, Recursive);
+			});
+			object.ids = child;
+		}
+	}
 }
